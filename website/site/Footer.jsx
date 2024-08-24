30538b31796988ee7d0a4f1d9593be7dcefa3666
Andy Wardley <abw@wardley.org>
2024-08-24 13:59:08 +0100

    Added badgerpower links to site header and footer

--- website/site/Footer.jsx
+++ website/site/Footer.jsx
@@ -1,14 +1,28 @@
 import React  from 'react'
+import Badger from '@/svg/badger-white.svg?react'
 import { version, date, years } from './Utils.jsx'
 
 const Footer = () =>
   <footer>
-    <div className="flex space">
+    <div className="flex space middle gap-2">
+      <a
+        href="https://badgerpower.com"
+        target="_blank"
+        rel="noreferrer"
+        className="badgerpower"
+      >
+        <Badger/>
+        Badger Powered
+      </a>
       <div>
-        &copy; Copyright {years} Andy Wardley
+        &copy; Copyright {years} {' '}
+        <a
+          href="https://github.com/abw"
+          target="_blank"
+          rel="noreferrer">Andy Wardley</a>
       </div>
       <div>
-        Version {version} {date}
+        v{version} - {date}
       </div>
     </div>
   </footer>
--- website/site/Header.jsx
+++ website/site/Header.jsx
@@ -1,10 +1,11 @@
 import React  from 'react'
 import Link   from './Link.jsx'
-import Icon  from '../ui/Icon.jsx'
-import { Toggle, useTheme } from '@abw/react-night-and-day'
-import Github from '../svg/github.svg?react'
-import { SIDEBAR, NO_SIDEBAR } from './Constants.jsx'
+import Icon  from '@/ui/Icon.jsx'
+import Github from '@/svg/github.svg?react'
+import Badger from '@/svg/badger-white.svg?react'
 import { version } from './Utils.jsx'
+import { Toggle, useTheme } from '@abw/react-night-and-day'
+import { SIDEBAR, NO_SIDEBAR } from './Constants.jsx'
 
 const Header = () => {
   const { variant, setVariant } = useTheme()
@@ -20,9 +21,19 @@ const Header = () => {
           <Link to="/" className="home mar-l-2" text="Badger Form"/>
           <span className="small mar-l-2">v{version}</span>
         </div>
-        <div>
-          <a href="https://github.com/abw/badger-form">
-            <Github className="night-and-day--icon mar-r-2"/>
+        <div className="flex middle gap-2">
+          <a
+            href="https://badgerpower.com/"
+            target="_blank"
+            rel="noreferrer">
+            <Badger className="badgerpower"/>
+          </a>
+          <a
+            href="https://github.com/abw/badger-form"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github className="night-and-day--icon"/>
           </a>
           <Toggle/>
         </div>
